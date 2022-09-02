8661e8c69550726063ac134f266d135c498a91e0
Saul <saul@relativity6.com>
2022-09-01 23:31:57 -0500

    - ADD second exercise

---
+++ 1.2- FizzBuzz.js
@@ -0,0 +1,21 @@
+for (let beginValue = 1; beginValue <= 100; beginValue++) {
+
+  if (!(beginValue % 3) && !(beginValue % 5)) {
+    console.log("FizzBuzz");
+  }
+
+  if (!(beginValue % 3) && beginValue % 5) {
+    console.log("Fizz");
+    continue
+  }
+
+  if (!(beginValue % 5) && beginValue % 3) {
+    console.log("Buzz");
+    continue
+  }
+
+  console.log(beginValue)
+
+}
+
+
