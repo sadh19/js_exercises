e70291a18f52b3bb004909b63bf9703e4e7ee1ae
sadh19 <saul@relativity6.com>
2023-06-05 19:48:31 -0600

    - ADD nth method

--- 3.3 - AList.js
+++ 3.3 - AList.js
@@ -5,6 +5,10 @@ function createListElement(value, rest) {
   };
 }
 
+function isTheLastListElement(element) {
+  return !element;
+}
+
 function arrayToList(arr) {
   let tempObject = null;
 
@@ -15,15 +19,52 @@ function arrayToList(arr) {
   return tempObject;
 }
 
-function listToArray() {
+function listToArray(list) {
+
+  let tempElement = list;
+  let arr = [];
+
+  while (!isTheLastListElement(tempElement)) {
+    arr = arr.concat(tempElement["value"]);
+    tempElement = tempElement["rest"];
+  }
+
+  return arr;
 
 }
 
-function prepend() {
-
+function prepend(element, list) {
+  return { value: element["value"], rest: list };
 }
 
-function nth() {
+function isTheElementRequired(element, index) {
+  return index === element["value"] - 1;
+}
+
+function isTheListOutOfElements(element) {
+  return !element;
+}
+
+function nth(list, index) {
+
+  let currentElement = list;
+  let foundedElement = undefined;
+  let isElementFounded = isTheElementRequired(list, index);
+  let isListOutOfElements = isTheListOutOfElements(list["rest"]);
+
+
+  while (!isListOutOfElements && !isElementFounded) {
+    currentElement = currentElement["rest"];
+    isListOutOfElements = isTheListOutOfElements(currentElement["rest"]);
+    isElementFounded = isTheElementRequired(currentElement, index);
+  }
+
+  if (isTheElementRequired(currentElement, index)) {
+    foundedElement = { ...currentElement };
+  }
+
+  return foundedElement;
+
 
 }
 
@@ -32,5 +73,10 @@ function nthRecursive() {
 }
 
 let testArray = [1, 2, 3];
+let testList = { value: 1, rest: { value: 2, rest: { value: 3, rest: { value: 4, rest: null } } } };
 
-console.log(arrayToList(testArray));
+
+//console.log(arrayToList(testArray));
+//console.log(listToArray(testList));
+//console.log(prepend({ value: 0 }, { value: 1, rest: { value: 2, rest: { value: 3, rest: null } } }));
+console.log(nth(testList, 7));
